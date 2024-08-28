eb29c8dba3a99dc30e62457ec477249438407413
Code-Funk-Newt <100172196+Code-Funk-Newt@users.noreply.github.com>
2024-08-28 15:19:25 -0700

    Unit1- lesson D

--- APretty_Unit_1/APretty_Unit-1_Lessons/Assets/Scripts/PlayerControler.cs
+++ APretty_Unit_1/APretty_Unit-1_Lessons/Assets/Scripts/PlayerControler.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
-    public float speed = 15.0f;
+
+    //rate of foward/backward movement
+    private float speed = 15.0f;
+    private float turnSpeed = 15.0f;
+    private float horizontalInput;
+    private float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +20,14 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+
         Debug.Log(Time.deltaTime);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        // turning vehicle 
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
 
 
     }
