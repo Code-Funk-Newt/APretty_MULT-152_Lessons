86949e227c1625b06852bc1f79f4eaca7546ac8b
Code-Funk-Newt <100172196+Code-Funk-Newt@users.noreply.github.com>
2024-10-26 21:29:50 -0700

    APretty Unit 4 B lesson

--- APretty_Unit_4/Assets/Scripts/PlayerController.cs
+++ APretty_Unit_4/Assets/Scripts/PlayerController.cs
@@ -25,7 +25,7 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         float magnitude = forwardInput * speed * Time.deltaTime;
-        rbPlayer.AddForce(focalPoint.transform.forward * magnitude, ForceMode.Impulse);
+        rbPlayer.AddForce(focalPoint.transform.forward * magnitude, ForceMode.Force);
 
         if(forwardInput > 0){
         rendererPlayer.material.color = new Color(1.0f - forwardInput , 1.0f, 1.0f - forwardInput); 
